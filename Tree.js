8c9b0d6037592b1eaa4d6d7ffc443207a3200e41
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-25 11:25:34 -0400

    Add tree class with buildTree() and includes() functions.

---
+++ Tree.js
@@ -0,0 +1,35 @@
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree(array);
+  }
+
+  buildTree(array) {
+    if (array.length === 0) return null; // if array is empty, there's nothing to make a node from, so return null
+
+    const uniqueArray = [...new Set(array)]; // remove duplicates b/c a Set can only hold unique values
+    const sortedArray = uniqueArray.sort((a, b) => a - b); // sort ascending
+
+    const midIndex = Math.floor(sortedArray.length / 2);
+    const root = new Node(sortedArray[midIndex]);
+
+    root.left = this.buildTree(sortedArray.slice(0, midIndex));
+    root.right = this.buildTree(sortedArray.slice(midIndex + 1));
+    return root;
+  }
+
+  includes(value) {
+    let currentNode = this.root; // starts at root
+    while (currentNode !== null) {
+      if (value === currentNode) {
+        return true;
+      }
+      if (value < currentNode) {
+        currentNode = currentNode.left;
+      }
+      if (value < currentNode) {
+        currentNode = currentNode.right;
+      }
+    }
+    return false;
+  }
+}
